0bf58609c2f4986c44b1dd355093b1be2e851ff0
Gjorgji Kirovski <dzolekirovski@gmail.com>
2024-02-15 15:40:40 +0100

    databinding and directives, to lecture 44

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,8 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName='Test server';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
 
   constructor() {
@@ -19,6 +21,8 @@ export class ServersComponent {
   }
 
   onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is :'+this.serverName;
   }
 
