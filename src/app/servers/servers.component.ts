4c9fcaf9cf8241ffee6f40c1c64e8af085a9f268
Gjorgji Kirovski <dzolekirovski@gmail.com>
2024-03-08 13:06:45 +0100

    base files for routing

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  public servers: {id: number, name: string, status: string}[] = [];
+
+  constructor(private serversService: ServersService) { }
+
+  ngOnInit() {
+    this.servers = this.serversService.getServers();
+  }
 
 }
