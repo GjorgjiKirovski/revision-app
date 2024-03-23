4ddf6ee210f97c9f8ae716322929b1997b4c778c
Gjorgji Kirovski <dzolekirovski@gmail.com>
2024-03-23 10:56:58 +0100

    template driven forms exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
-  styles:[`
-    h3{
-      color:dodgerblue;
-    }
-  `]
+  styleUrls:['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') myForm: NgForm
+
+  subscriptions= ['Basic', 'Advanced', 'Pro']
+
+  selectedSubscription = 'Advanced';
+
+  onSubmit(){
+    console.log(this.myForm.value)
+  }
 }
