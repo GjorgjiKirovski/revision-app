80428272ef4a3167e261212fdb5cb1fde785e9d5
Gjorgji Kirovski <dzolekirovski@gmail.com>
2024-03-27 23:57:43 +0100

    pipes section finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from '@angular/forms'
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
+
 @NgModule({
   declarations: [
     AppComponent,
+    ShortenPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
