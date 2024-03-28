948fc052865d115ac2a575d063809cb2d6d10cd2
Gjorgji Kirovski <dzolekirovski@gmail.com>
2024-03-28 01:35:09 +0100

    pipe exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from '@angular/forms'
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
+import { ReversePipe } from './reverse.pipe';
+import { OrderPipe } from './order.pipe';
+
 @NgModule({
   declarations: [
     AppComponent,
+    ReversePipe,
+    OrderPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/order.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'order'
+})
+export class OrderPipe implements PipeTransform {
+
+  transform(value: any): any {
+    console.log(value);
+    return value.sort((a,b)=>{
+      const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      // names must be equal
+      return 0;
+    });
+  }
+
+}
